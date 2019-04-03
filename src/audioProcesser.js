51fbfb4f07dde5d02049ae844881a0f58fcb66fb
yusuke_ten <net.escape.com@gmail.com>
2019-04-03 23:34:35 +0900

    update

--- src/audioProcesser.js
+++ src/audioProcesser.js
@@ -4,13 +4,58 @@ export default class {
     this.audioCtx = new AudioContext()
     this.source = this.audioCtx.createBufferSource()
     this.analyser = this.audioCtx.createAnalyser()
-
-    this.audioCtx.decodeAudioData(audioData).then(buffer => {
-      // デコードしたデータをここで使う
-      console.log(buffer)
+    this.analyser.minDecibels = -90
+    this.analyser.maxDecibels = -10
+    this.analyser.smoothingTimeConstant = 0.85
+    this.analyser.fftSize = 256
+    this.canvas = document.querySelector('#canvas')
+    this.canvas.width = 500
+    this.canvas.height = 500
+    this.canvasCtx = this.canvas.getContext('2d')
+    this.decodeAudio(audioData)
+  }
+  decodeAudio(audioData) {
+    this.audioCtx.decodeAudioData(audioData, buffer => {
+      console.log(this)
+      console.log(this.source)
       this.source.buffer = buffer
-
-      this.source.connect(this.audioCtx.destination)
+      this.source.connect(this.analyser)
+      this.analyser.connect(this.audioCtx.destination)
+      this.bufferLength = this.analyser.frequencyBinCount
+      this.dataArray = new Uint8Array(this.bufferLength)
+      this.canvasCtx.clearRect(0, 0, 500, 500)
+      this.draw()
+      this.source.start(0)
     })
+  }
+  temp() {}
+  draw() {
+    let WIDTH = 500
+    let HEIGHT = 500
+    console.log(this)
+    let drawVisual = requestAnimationFrame(this.draw)
+
+    this.analyser.getByteFrequencyData(this.dataArray)
+
+    this.canvasCtx.fillStyle = 'rgb(0, 0, 0)'
+    this.canvasCtx.fillRect(0, 0, WIDTH, HEIGHT)
+
+    var barWidth = (WIDTH / this.bufferLength) * 2.5
+    var barHeight
+    var x = 0
+
+    for (var i = 0; i < this.bufferLength; i++) {
+      barHeight = this.dataArray[i]
+
+      this.canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)'
+      this.canvasCtx.fillRect(
+        x,
+        HEIGHT - barHeight / 2,
+        barWidth,
+        barHeight / 2
+      )
+
+      x += barWidth + 1
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 import * as dat from 'dat.gui'
 import fft from './fft'
+import audioProcesser from './audioProcesser'
 // import audioProcesser from './audioProcesser'
-window.AudioContext = window.AudioContext || window.webkitAudioContext
-const audioCtx = new AudioContext()
-const source = audioCtx.createBufferSource()
-const analyser = audioCtx.createAnalyser()
-analyser.minDecibels = -90
-analyser.maxDecibels = -10
-analyser.smoothingTimeConstant = 0.85
-let dataArray
-let canvasCtx
-let bufferLength
 
 function init() {
-  var canvas = document.querySelector('#canvas')
-  canvas.width = 500
-  canvas.height = 500
-
-  canvasCtx = canvas.getContext('2d')
 
   const elm = document.querySelector('#start')
   elm.addEventListener('click', function() {
@@ -29,47 +15,7 @@ function init() {
     // Decode asynchronously
     request.onload = function() {
       var audioData = request.response
-      audioCtx.decodeAudioData(audioData).then(buffer => {
-        // デコードしたデータをここで使う
-        source.buffer = buffer
-        source.connect(analyser)
-        analyser.connect(audioCtx.destination)
-        analyser.fftSize = 256
-        bufferLength = analyser.frequencyBinCount
-        dataArray = new Uint8Array(bufferLength)
-
-        canvasCtx.clearRect(0, 0, 500, 500)
-        function draw() {
-          let WIDTH = 500
-          let HEIGHT = 500
-          let drawVisual = requestAnimationFrame(draw)
-
-          analyser.getByteFrequencyData(dataArray)
-
-          canvasCtx.fillStyle = 'rgb(0, 0, 0)'
-          canvasCtx.fillRect(0, 0, WIDTH, HEIGHT)
-
-          var barWidth = (WIDTH / bufferLength) * 2.5
-          var barHeight
-          var x = 0
-
-          for (var i = 0; i < bufferLength; i++) {
-            barHeight = dataArray[i]
-
-            canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)'
-            canvasCtx.fillRect(
-              x,
-              HEIGHT - barHeight / 2,
-              barWidth,
-              barHeight / 2
-            )
-
-            x += barWidth + 1
-          }
-        }
-        draw()
-        source.start(0)
-      })
+      const audio = new audioProcesser(audioData)
     }
     request.send()
   })
